6fa1c21dc5e81d5ecf1ac21048f258fca8daebfc
CePseudo <hoffmann.axel.6032@gmail.com>
2025-04-11 10:46:36 +0200

    added eduperson in scope

--- src/driver.ts
+++ src/driver.ts
@@ -58,7 +58,7 @@ export class UlbOidcDriver
 
   protected configureRedirectRequest(request: RedirectRequestContract<UlbOidcScopes>) {
     request.param('response_type', 'code')
-    request.scopes(['openid', 'profile', 'email'])
+    request.scopes(['openid', 'profile', 'email', 'eduperson'])
   }
 
   accessDenied() {
